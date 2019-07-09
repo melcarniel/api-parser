601cc5a42246f3c22c0d9dbe3eaf1ea4751104ac
Melina Carniel <melinagarciac@gmail.com>
2019-07-09 16:30:16 -0300

    Rota por id do game

--- routes/gamesRoute.js
+++ routes/gamesRoute.js
@@ -28,4 +28,30 @@ router.get('/', async (req, res) => {
     }
 });
 
+//@route  GET /games/:game_id
+//@desc   Pegar relatório por jogo usando o id
+//@access Public
+router.get('/:game_id', async (req, res) => {
+    try {
+        let id = req.params.game_id;
+        fs.readFile('./dados/games.log', (err, log) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({ error: err });
+            }
+
+            let text = log.toString();
+            let result = Parser(text);
+            if(result.length-1 < id){
+                res.send('Jogo não existe!')
+            }
+            res.json(result[id]);
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Erro de servidor!')
+    }
+});
+
 module.exports = router;
