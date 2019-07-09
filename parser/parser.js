1b620a0266b58434e493c777cd5ff70928e00ce0
Melina Carniel <melinagarciac@gmail.com>
2019-07-09 11:21:26 -0300

    array dos players do jogo

--- parser/parser.js
+++ parser/parser.js
@@ -22,6 +22,23 @@ function parserRead(log) {
                 });
 
                 break;
+            case userGame:
+
+                let index1 = linha.indexOf('n\\');
+                let index2 = linha.indexOf('\\t') - 1;
+                let user = linha.trim().substr(index1, index2 - index1);
+                user = user.replace(/\\/g, '');
+
+                if (gamesArray[count - 1].game.players.length > 0) {
+                    let index = gamesArray[count - 1].game.players.findIndex(item => user == item);
+                    if (index == -1) {
+                        gamesArray[count - 1].game.players.push(user);
+                    }
+                } else {
+                    gamesArray[count - 1].game.players.push(user);
+                }
+
+                break;
 
         }
     });
